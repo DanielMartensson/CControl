67d3c432dde6f888a56ef9417e495e5c80befeda
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-02-29 17:27:04 +0100

    Update Feedback.c

--- src/CControl/Sources/Feedback.c
+++ src/CControl/Sources/Feedback.c
@@ -204,6 +204,35 @@ static void integral(float* xi, float* r, float* y) {
 	}
 }
 
+/*
+ * This uses the dlyap function to check the stability
+ * A need to be square. Returns 0 when system is not stable.
+ * Returns 1 when system is stable.
+ */
+int stability(float* A){
+	float P[ADIM*ADIM];
+
+	// Create a symmetric matrix that is a positive definite matrix
+	float Q[ADIM*ADIM];
+	for(int i = 0; i < ADIM; i++)
+		for(int j = 0; j < ADIM; j++)
+			P[ADIM*i + j] = i + j + 1;
+	dlyap(A, P, Q, ADIM);
+
+	// Check if P is symmetric and positive definite
+	for(int i = 0; i < ADIM; i++){
+		for(int j = i; j < ADIM; j++){
+			if(i == j){
+				if(P[ADIM*i + j] <= 0) return 0; // Diagonal
+			}else{
+				if(P[ADIM*i + j] <= 0 && P[ADIM*j + i] <= 0) return 0; // non-Diagonal
+			}
+		}
+	}
+
+	return 1;
+}
+
 
 /*
  * Solve U from R = PHI*x + GAMMA*U
