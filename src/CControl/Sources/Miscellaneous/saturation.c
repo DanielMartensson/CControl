028c51243bb6f158cf37695169e99e6d90aa4b2f
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-07-21 18:38:29 +0200

    Update saturation.c

--- src/CControl/Sources/Miscellaneous/saturation.c
+++ src/CControl/Sources/Miscellaneous/saturation.c
@@ -10,10 +10,12 @@
 /*
  * This will saturate the input value with limits
  */
-void saturation(float* input, float lower_limit, float upper_limit){
-	if(*input > upper_limit){
-		*input = upper_limit;
-	}else if(*input < lower_limit){
-		*input = lower_limit;
-	}// else = nothing happens to *input
+float saturation(float input, float lower_limit, float upper_limit){
+	if(input > upper_limit){
+		return upper_limit;
+	}else if(input < lower_limit){
+		return lower_limit;
+	}else{
+		return input; // No action
+	}
 }
