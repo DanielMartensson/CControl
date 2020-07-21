94fa16d346f311d578a927db7b6d0a194ab37f7e
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-07-21 18:37:03 +0200

    Update mrac.c

--- src/CControl/Sources/ControlEngineering/mrac.c
+++ src/CControl/Sources/ControlEngineering/mrac.c
@@ -1,82 +1,66 @@
 /*
  * mrac.c
  *
- *  Created on: 1 mars 2020
+ *  Created on: 21 Juli 2020
  *      Author: Daniel Mårtensson
  */
 
 
 #include "../../Headers/Functions.h"
 
-static void integral(uint8_t ANTI_WINDUP, float* xi, float* r, float* y, uint8_t RDIM);
+static void integral(float* I, float gain, float* x, float *e, uint8_t RDIM);
+static void saturate(float* I, float limit, uint8_t RDIM);
+static void modelerror(float* e, float* y, float* r, uint8_t RDIM);
+static void findinput(float* u, float* r, float* I1, float* y, float* I2, uint8_t RDIM);
 
 /*
- * This is Adaptive Model Reference Control - No need to use a transfer function here
+ * This is Adaptive Model Reference Control - We assume that the reference model is Gm(s) = 1.
  *
- * u = -LEARNING*r*integral(r*y - r*r) - LEARNING*y*integral(y*y - y*r);
+ * u = r*integral(-gain*r*e) - y*integral(gain*y*e);
  * Where: u = input to our system
  *      : r = reference
  *      : y = output
- *      : LEARNING = learning factor e.g 0.00001
- *      : K1 = integral(r*e) = integral(r*y - r*r)
- *      : K2 = integral(y*e) = integral(y*y - y*r);
- *      : e = Model error y - r
+ *      : gain = learning factor e.g 0.00001
+ *      : e = Model error y-r
  *
  * This can be used with multiple outputs and references
  * HINT: Look up my repository Adaptive-Control and look for Model Reference Adaptive Control with Lyapunov rule
  * HINT: The book Adaptive Control by Karl-Johan Åström describe on page 208 how this works. ISBN: 9780486462783
  */
-void mrac(uint8_t ANTI_WINDUP, float LEARNING, float* y, float* u, float* r, float* K1, float* K2, uint8_t RDIM){
+void mrac(float limit, float gain, float* y, float* u, float* r, float* I1, float* I2, uint8_t RDIM){
 
-	// First create these vectors
-	float ry[RDIM];
-	float rr[RDIM];
-	float yy[RDIM];
-	float yr[RDIM];
+	// Find the model error
+	float e[RDIM];
+	modelerror(e, y, r, RDIM);
 
-	// Then compute the product
-	for(int i = 0; i < RDIM; i++){
-		*(ry + i) = *(r + i) * *(y + i);
-		*(rr + i) = *(r + i) * *(r + i);
-		*(yy + i) = *(y + i) * *(y + i);
-		*(yr + i) = *(y + i) * *(r + i);
-	}
+	// Integrate
+	integral(I1, -gain, r, e, RDIM); // I1 = I1 - gain*r*e
+	integral(I2, gain, y, e, RDIM); // I2 = I2 + gain*y*e
 
-	// Integrate K = K + y - r
-	integral(ANTI_WINDUP, K1, ry, rr, RDIM);
-	integral(ANTI_WINDUP, K2, yy, yr, RDIM);
+	// Saturate
+	saturate(I1, limit, RDIM);
+	saturate(I2, limit, RDIM);
 
-	// Compute u now
-	for(int i = 0; i < RDIM; i++){
-		*(u + i) = -LEARNING * *(r + i) * *(K1 + i) - LEARNING * *(y + i) * *(K2 + i);
-	}
+	// Find input signal
+	findinput(u, r, I1, y, I2, RDIM);
 }
 
-/*
- * This computes the integral by sum state vector xi with reference - measurement
- * xi = xi + r - y;
- */
-static void integral(uint8_t ANTI_WINDUP, float* xi, float* r, float* y, uint8_t RDIM) {
-	for(int i = 0; i < RDIM; i++){
-		/*
-		 * Anti-windup
-		 */
-		if(ANTI_WINDUP == 0){
-			*(xi + i) = *(xi + i) + *(r + i) - *(y + i); // Always integrate
-		}else if(ANTI_WINDUP == 1){
-			if(*(r + i) > *(y + i)){
-				*(xi + i) = *(xi + i) + *(r + i) - *(y + i); // Only integrate when r > y, else delete
-			}else{
-				*(xi + i) = 0; // Delete just that xi
-			}
-		}else if(ANTI_WINDUP == 2){
-			if(*(r + i) > *(y + i)){
-				*(xi + i) = *(xi + i) + *(r + i) - *(y + i); // Only integrate r > y, else stop
-			}
-		}else{
-			*(xi + i) = *(xi + i) + *(r + i) - *(y + i); // Always integrate if nothing else selected
-		}
-	}
+static void integral(float* I, float gain, float* x, float *e, uint8_t RDIM){
+	for(uint8_t i = 0; i < RDIM; i++)
+		*(I + i) += gain* *(x + i) * *(e + i);
 }
 
+static void saturate(float* I, float limit, uint8_t RDIM){
+	for(uint8_t i = 0; i < RDIM; i++)
+		*(I + i) = saturation(*(I + i), limit, limit);
+}
 
+static void modelerror(float* e, float* y, float* r, uint8_t RDIM){
+	for(uint8_t i = 0; i < RDIM; i++)
+		*(e + i) = *(y + i) - *(r + i);
+}
+
+static void findinput(float* u, float* r, float* I1, float* y, float* I2, uint8_t RDIM){
+	for(uint8_t i = 0; i < RDIM; i++)
+		*(u + i) = *(r + i) * *(I1 + i) - *(y + i) * *(I2 + i);
+}
