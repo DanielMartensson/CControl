62c0d28863666182ef2beb761b8df1f6712da097
Daniel Mårtensson <daniel.martensson100@outlook.com>
2020-08-06 00:52:04 +0200

    Update stability.c

--- src/CControl/Sources/ControlEngineering/stability.c
+++ src/CControl/Sources/ControlEngineering/stability.c
@@ -9,18 +9,18 @@
 
 /*
  * Check the stability of the matrix A by checking the eigenvalues
- * Return 1 if A is stable.
- * Return 0 is A is unstable
+ * Return true if A is stable.
+ * Return false is A is unstable
  */
-uint8_t stability(float* A, uint8_t ADIM){
+bool stability(float* A, uint8_t ADIM){
 	float wr[ADIM]; // Real eigenvalues
 	float wi[ADIM]; // Imaginary eigenvalues
 	eig(A, wr, wi, ADIM);
-	uint8_t stable = 1; // Assume that the system is stable
+	bool stable = true; // Assume that the system is stable
 	for(uint8_t i = 0; i < ADIM; i++){
 		float abs_value = sqrtf(wr[i]*wr[i] + wi[i]*wi[i]);
 		if(abs_value > 1){
-			stable = 0; // Unstable
+			stable = false;
 		}
 	}
 	return stable;
