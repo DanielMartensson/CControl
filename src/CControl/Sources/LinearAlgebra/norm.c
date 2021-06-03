f8978389d87c6b9656af16830b7f8f679b366639
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-03 23:57:24 +0200

    Added L1 and L2 norms for both matrix + vector

--- src/CControl/Sources/LinearAlgebra/norm.c
+++ src/CControl/Sources/LinearAlgebra/norm.c
@@ -10,26 +10,56 @@
 /*
  * Find the norm of A
  * A[m*n]
- * l = 1 - 1-norm, the largest column sum of the absolute values of A.
+ * l = 1 = L1-norm
+ * l = 2 = L2-norm
  */
 float norm(float A[], uint32_t row, uint32_t column, uint8_t l){
 	if(l == 1){
-		// MATLAB: sum(A, 1)
-		for (uint32_t i = 1; i < row; i++) {
-			for (uint32_t j = 0; j < column; j++) {
-				A[j] += fabsf(A[i * column + j]);
+		// Vector
+		if(row == 1){
+			float sum_sqrt = 0;
+			for(uint32_t i = 0; i < column; i++)
+				sum_sqrt += sqrtf(A[i]*A[i]);
+			return sum_sqrt;
+		}else{
+			// Matrix
+			// MATLAB: sum(A, 1)
+			for (uint32_t i = 1; i < row; i++) {
+				for (uint32_t j = 0; j < column; j++) {
+					A[j] += fabsf(A[i * column + j]);
+				}
 			}
-		}
-		// Find the largest value on row 0
-		float maxValue = A[0];
-		for (uint32_t j = 1; j < column; j++) {
-			if(A[j] > maxValue){
-				maxValue = A[j];
+
+			// Find the largest value on row 0
+			float maxValue = A[0];
+			for (uint32_t j = 1; j < column; j++) {
+				if(A[j] > maxValue){
+					maxValue = A[j];
+				}
 			}
+			return maxValue;
 		}
-		return maxValue;
+	}
+	else if(l == 2){
+		// Vector
+		if(row == 1){
+			float sqrt_sum = 0;
+			for(uint32_t i = 0; i < column; i++)
+				sqrt_sum += A[i]*A[i];
+			return sqrtf(sqrt_sum);
+		}else{
+			// Matrix
+			float U[row*column];
+			float S[column];
+			float V[column*column];
+			svd_golub_reinsch(A, row, column, U, S, V);
+			float max_singular_value = 0;
+			for(uint32_t i = 0; i < column; i++)
+				if(S[i] > max_singular_value)
+					max_singular_value = S[i];
+			return max_singular_value;
+		}
 	}
 	return 0;
 	/* add more norms here */
 }
-
