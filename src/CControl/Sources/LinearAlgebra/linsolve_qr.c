53274de0939742229c78812cf0fd3faa1a53fd7c
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-10 15:59:50 +0200

    Create linsolve_qr.c

---
+++ src/CControl/Sources/LinearAlgebra/linsolve_qr.c
@@ -0,0 +1,25 @@
+/*
+ * linsolve_qr.c
+ *
+ *  Created on: 10 juni 2021
+ *      Author: Daniel Mårtensson
+ */
+
+#include "../../Headers/Functions.h"
+
+/*
+ * Solve Ax=b with QR decomposition
+ * A[m*n]
+ * b[m]
+ * x[n]
+ */
+void linsolve_qr(float* A, float* x, float* b, int row, int column){
+	// QR-decomposition
+	float Q[row*row];
+	float R[row*column];
+	qr(A, Q, R, row, column);
+	tran(Q, row, row); // Do transpose Q -> Q^T
+	float QTb[row];
+	mul(Q, b, QTb, row, row, 1); // Q^Tb = Q^T*b
+	linsolve_upper_triangular(R, x, QTb, column);
+}
