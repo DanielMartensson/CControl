543c4c71f5174877bc9234603f8842d9c36eb7e5
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-10 15:20:54 +0200

    Create linsolve_chol.c

---
+++ src/CControl/Sources/LinearAlgebra/linsolve_chol.c
@@ -0,0 +1,25 @@
+/*
+ * linsolve_chol.c
+ *
+ *  Created on: 10 juni 2021
+ *      Author: Daniel Mårtensson
+ */
+
+
+#include "../../Headers/Functions.h"
+
+/*
+ * This solves Ax=b with Cholesky decomposition
+ * A [m*n] (symmetric and positive definite)
+ * x [n]
+ * b [m]
+ * n == m
+ */
+void linsolve_chol(float *A, float *x, float *b, int row) {
+	float L[row*row];
+	uint8_t status = chol(A, L, row);
+	float y[row];
+	linsolve_lower_triangular(L, y, b, row);
+	tran(L, row, row);
+	linsolve_upper_triangular(L, x, y, row);
+}
