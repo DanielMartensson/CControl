2e37f8e3657cb04f3a175eb6f4dc8b601615ffcd
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-27 16:23:11 +0200

    Update print.c

--- src/CControl/Sources/Miscellaneous/print.c
+++ src/CControl/Sources/Miscellaneous/print.c
@@ -10,9 +10,9 @@
 /*
  * Print matrix or vector - Just for error check
  */
-void print(float A[], int row, int column) {
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < column; j++) {
+void print(float A[], uint32_t row, uint32_t column) {
+	for (uint32_t i = 0; i < row; i++) {
+		for (uint32_t j = 0; j < column; j++) {
 			printf("%0.18f\t", *(A++));
 		}
 		printf("\n");
