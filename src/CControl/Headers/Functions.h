743fb4f6dfdd83674faf18b55221a5bf89ab4a75
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-27 16:02:15 +0200

    Update Functions.h

--- src/CControl/Headers/Functions.h
+++ src/CControl/Headers/Functions.h
@@ -58,7 +58,7 @@ void ukf(float xhat[], float zk[], float u[], float P[], float Q[], float R[], f
  */
 uint8_t inv(float* A, int row);
 void linsolve_upper_triangular(float* A, float* x, float* b, int column);
-void tran(float* A, uint32_t row, uint32_t column);
+void tran(float A[], uint32_t row, uint32_t column);
 void mul(float A[], float B[], float C[], uint32_t row_a, uint32_t column_a, uint32_t column_b);
 void svd_jacobi_one_sided(float *A, uint16_t row, uint8_t max_iterations, float *U, float *S, float *V);
 void dlyap(float* A, float* P, float* Q, int row);
