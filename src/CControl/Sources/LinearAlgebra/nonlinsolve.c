afafd41c32992b5e6edaf2541d39a6b86b43b413
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-27 16:38:17 +0200

    Update nonlinsolve.c

--- src/CControl/Sources/LinearAlgebra/nonlinsolve.c
+++ src/CControl/Sources/LinearAlgebra/nonlinsolve.c
@@ -2,14 +2,14 @@
  * nonlinsolve.c
  *
  *  Created on: 5 june 2021
- *      Author: Daniel MÃ¥rtensson
+ *      Author: Daniel MÃƒÂ¥rtensson
  */
 
 #include "../../Headers/Functions.h"
 
-static float check_solution(float* dx, float* x, float* past_sqrt_sum_dx, float* best_x, uint8_t* elements);
+static float check_solution(float dx[], float x[], float* past_sqrt_sum_dx, float best_x[], uint8_t* elements);
 
-void nonlinsolve(void (*nonlinear_equation_system)(float*, float*, float*), float* b, float* x, uint8_t elements, float alpha, float max_value, float min_value, bool random_guess_active){
+void nonlinsolve(void (*nonlinear_equation_system)(float[], float[], float[]), float b[], float x[], uint8_t elements, float alpha, float max_value, float min_value, bool random_guess_active){
 	// Initial parameters and arrays
 	float dx[elements];
 	float best_sqrt_sum_dx = FLT_MAX;
@@ -31,7 +31,7 @@ void nonlinsolve(void (*nonlinear_equation_system)(float*, float*, float*), floa
 		for(uint32_t i = 0; i < random_iterations; i++){
 			// Init x with random values between min_value and max_value
 			for(uint8_t j = 0; j < elements; j++)
-				*(x + j) = difference_value*((float) rand() / RAND_MAX) + min_value;
+				x[j] = difference_value*((float) rand() / RAND_MAX) + min_value;
 
 			// Simulate the nonlinear system
 			(*nonlinear_equation_system)(dx, b, x);
@@ -59,9 +59,8 @@ void nonlinsolve(void (*nonlinear_equation_system)(float*, float*, float*), floa
 		past_sqrt_sum_dx = sqrt_sum_dx;
 
 		// Update the vector x using stochastic gradient descent
-		for(uint8_t j = 0; j < elements; j++){
-			*(x + j) = *(x + j) - (alpha + past_gradients[gradient_index]) * *(dx + j); // x = x + alpha * dx
-		}
+		for(uint8_t j = 0; j < elements; j++)
+			x[j] -= (alpha + past_gradients[gradient_index]) * dx[j]; // x = x - alpha * dx
 		past_gradients[gradient_index] = alpha * norm(dx, 1, elements, 2); // Save the last for next time.
 		gradient_index++;
 		if(gradient_index >= maximum_gradients_index)
@@ -72,7 +71,7 @@ void nonlinsolve(void (*nonlinear_equation_system)(float*, float*, float*), floa
 	memcpy(x, best_x, sizeof(float)*elements);
 }
 
-static float check_solution(float* dx, float* x, float* best_sqrt_sum_dx, float* best_x, uint8_t* elements){
+static float check_solution(float dx[], float x[], float* best_sqrt_sum_dx, float best_x[], uint8_t* elements){
 	// Do L2-norm on dx
 	float sqrt_sum_dx = norm(dx, 1, *elements, 2);
 
