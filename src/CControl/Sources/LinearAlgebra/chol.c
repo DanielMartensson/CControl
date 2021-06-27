6326db0525a5dcf6e3f194ebbba5aa062da35930
Daniel Mårtensson <daniel.martensson100@outlook.com>
2021-06-27 15:47:27 +0200

    Update chol.c

--- src/CControl/Sources/LinearAlgebra/chol.c
+++ src/CControl/Sources/LinearAlgebra/chol.c
@@ -14,19 +14,19 @@
  * L [m*n]
  * n == m
  */
-void chol(float *A, float *L, int row) {
+void chol(float A[], float L[], int row) {
 	memset(L, 0, row*row*sizeof(float));
 	for (int i = 0; i < row; i++)
 		for (int j = 0; j < (i + 1); j++) {
 			float s = 0;
 			for (int k = 0; k < j; k++)
-				s += *(L + row * i + k) * *(L + row * j + k);
+				s += L[row * i + k] * L[row * j + k];
 
 			// We cannot divide with zero
-			if (*(L + row * j + j) == 0) {
-				*(L + row * j + j) = FLT_EPSILON; // Same as eps command in MATLAB
+			if (L[row * j + j] == 0) {
+				L[row * j + j] = FLT_EPSILON; // Same as eps command in MATLAB
 			}
-			*(L + row * i + j) = (i == j) ? sqrtf(*(A + row * i + i) - s) : (1.0 / *(L + row * j + j) * (*(A + row * i + j) - s));
+			L[row * i + j] = (i == j) ? sqrtf(A[row * i + i] - s) : (1.0 / L[row * j + j] * (A[row * i + j] - s));
 		}
 }
 
