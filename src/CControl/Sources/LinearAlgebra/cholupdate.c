a443d3608ebac90a99cb90c3ae4f698c5bd11e8c
danielmartensson <daniel.martensson100@outlook.com>
2021-12-14 21:16:23 +0100

    Fixed cholupdate.c

--- src/CControl/Sources/LinearAlgebra/cholupdate.c
+++ src/CControl/Sources/LinearAlgebra/cholupdate.c
@@ -10,54 +10,49 @@
 /*
  * Create L = cholupdate(L, x, rank_one_update)
  * L is a lower triangular matrix with real and positive diagonal entries from cholesky decomposition L = chol(A)
- * A [m*n]
+ * L [m*n]
  * x [m]
  * n == m
  */
-void cholupdate(float L[], float x[], uint16_t row, bool rank_one_update) {
-	uint16_t k, i;
-	int8_t F = -1;
-	float r, c, s, a, b;
-	if (rank_one_update)
-		F = 1;
-	for (k = 0; k < row; k++) {
-		a = L[k * row + k];
-		b = x[k];
-		r = sqrtf(a * a + F * b * b);
-		c = r / a;
-		s = b / a;
-		L[k * row + k] = r;
-		if (k < row) {
-			for (i = k + 1; i < row; i++) {
-				L[k * row + i] = (L[k * row + i] + F * s * x[i]) / c;
-				x[i] = c * x[i] - s * L[k * row + i];
+void cholupdate(float L[], float x[], uint16_t row, bool rank_one_update){
+	float alpha = 0.0, beta = 1.0, beta2 = 0.0, gamma = 0.0, delta = 0.0;
+	tran(L, row, row);
+
+	for(uint8_t i = 0; i < row; i++){
+		alpha = x[i] / L[row*i + i];
+		beta2 = rank_one_update == true ? sqrtf(beta * beta + alpha*alpha) : sqrtf(beta * beta - alpha*alpha);
+		gamma = rank_one_update == true ? alpha / (beta2 * beta) : -alpha / (beta2 * beta);
+
+		if(rank_one_update){
+			// Update
+			delta = beta / beta2;
+			L[row*i + i] = delta * L[row*i + i] + gamma * x[i];
+
+			if(i < row){
+				for(uint8_t k = i+1; k < row; k++)// line 34 in tripfield chol_updown function
+					x[k] -= alpha * L[row*k + i];
+
+				for(uint8_t k = i+1; k < row; k++)// line 35 in tripfield chol_updown function
+					L[row*k + i] = delta * L[row*k + i] + gamma * x[k];
 			}
+			x[i] = alpha;
+			beta = beta2;
+		}else{
+			// Downdate
+			delta = beta2 / beta;
+			L[row*i + i] = delta * L[row*i + i];
+
+			if(i < row){
+				for(uint8_t k = i+1; k < row; k++)
+					x[k] -= alpha * L[row*k + i];
+
+				for(uint8_t k = i+1; k < row; k++)
+					L[row*k + i] = delta * L[row*k + i] + gamma * x[k];
+			}
+			x[i] = alpha;
+			beta = beta2;
 		}
 	}
-}
 
-/*
- * GNU Octave code:
- * function [L] = cholupdate2(L, x, OP)
-	  cholupdate(L, x, OP) % OP can be "+" = rank_one_update or "-" = rank_one_downdate
-	  F = 1;
-	  if(strcmp(OP, '-') == 1)
-		F = -1; % Rank_one_downdate
-	  end
-	  n = length(x);
-	  for k = 1:n
-		  a = L(k,k);
-		  b = x(k);
-		  r = sqrt(a*a + F * b*b);
-		  c = r / a;
-		  s = b / a;
-		  L(k, k) = r;
-		  if k < n
-			  for i = k+1:n
-				L(k, i) = (L(k,i) + F * s * x(i)) / c;
-				x(i) = c * x(i) - s * L(k, i);
-			  end
-		  end
-	  end
-	end
- */
+	tran(L, row, row);
+}
