6b55522bcecabddd41acf04b49accdc3f3c9ade4
danielmartensson <daniel.martensson100@outlook.com>
2021-12-28 17:00:08 +0100

    false to 0

--- src/CControl/Sources/ArtificialIntelligence/inpolygon.c
+++ src/CControl/Sources/ArtificialIntelligence/inpolygon.c
@@ -32,7 +32,7 @@ uint8_t inpolygon(float x, float y, float px[], float py[], uint8_t p){
 	}
 
 	/* Do a better check */
-	uint8_t ok = false;
+	uint8_t ok = 0;
 	uint8_t i = 0;
 	uint8_t j = p - 1;
 	while (i < p) {
