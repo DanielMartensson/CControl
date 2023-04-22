31bd6dc378bd929bacab483e2eed3576cbd5a268
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-04-22 19:06:50 +0200

    Added 0.0f

--- src/CControl/Sources/Miscellaneous/sign.c
+++ src/CControl/Sources/Miscellaneous/sign.c
@@ -12,9 +12,9 @@
   */
 float sign(float number) {
 	float s;
-	if (number > 0) {
+	if (number > 0.0f) {
 		s = 1.0f; 
-	}else if (number < 0) {
+	}else if (number < 0.0f) {
 		s = -1.0f; 
 	}else{
 		s = 0.0f;
