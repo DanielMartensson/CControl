e9f4d66d2f83764c353bce214b451aa28037e2bf
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-04-26 19:13:49 +0200

    Brackets an uint16_t

--- src/CControl/Sources/LinearAlgebra/det.c
+++ src/CControl/Sources/LinearAlgebra/det.c
@@ -21,19 +21,24 @@ float det(float A[], uint16_t row) {
 	float *LU = (float*)malloc(row * row * sizeof(float));
 	uint8_t *P = (uint8_t*)malloc(row * row * sizeof(uint8_t));
 	uint8_t status = lup(A, LU, P, row);
-	if(status == 0)
+	if (status == 0) {
 		return 0; /* matrix is singular */
+	}
 
-	for (i = 0; i < row; ++i)
+	for (i = 0; i < row; ++i) {
 		determinant *= LU[row * P[i] + i];
+	}
 
-	int j = 0;
-	for (i = 0; i < row; ++i)
-		if (P[i] != i)
+	uint16_t j = 0;
+	for (i = 0; i < row; ++i) {
+		if (P[i] != i) {
 			++j;
+		}
+	}
 
-	if (j && (j - 1) % 2 == 1)
+	if (j && (j - 1) % 2 == 1) {
 		determinant = -determinant;
+	}
 
 	/* Free */
 	free(LU);
