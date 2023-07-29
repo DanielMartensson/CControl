5880ea2997b3bac55da56b8a4c70dd4887cb0f82
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-07-29 13:29:41 +0200

    Update PCA - Better

--- src/CControl/Sources/Statistics/pca.c
+++ src/CControl/Sources/Statistics/pca.c
@@ -7,85 +7,63 @@
 
 #include "../../Headers/functions.h"
 
-static void center_data(float X[], float mu[], size_t row, size_t column);
 static void compute_components(float X[], float W[], size_t components, size_t row, size_t column);
 
 /*
  * Principal Component Analysis (PCA)
  * This compute the PCA of X and return components W
  * X[m*n]
- * W[n*components]
- * P[m*components]
+ * W[m*c]
+ * P[c*n]
  * mu[n]
  */
-void pca(float X[], float W[], float P[], float mu[], size_t components, size_t row, size_t column) {
-	/* Copy the data X -> Z */
-	float* Z = (float*)malloc(row * column * sizeof(float));
-	memcpy(Z, X, row * column * sizeof(float));
+void pca(float X[], float W[], float P[], float mu[], size_t c, size_t row, size_t column) {
+	/* Copy the data X -> Y */
+	float* Y = (float*)malloc(row * column * sizeof(float));
+	memcpy(Y, X, row * column * sizeof(float));
 
-	/* Average and center data Z = Z - mean(Z) */
-	center_data(Z, mu, row, column);
-
-	/* Do covariance Y = cov(Z) */
-	float* Y = (float*)malloc(column * column * sizeof(float));
-	covm(Z, Y, row, column);
+	/* Average and center data Y = Y - mean(Y) */
+	center(Y, mu, row, column);
 
 	/* Get components [U, S, V] = svd(Y) */
-	compute_components(Y, W, components, column, column);
+	compute_components(Y, W, c, row, column);
 
-	/* Project P = Z*W */
-	mul(Z, W, P, row, column, components);
+	/* Transpose W */
+	float* Wt = (float*)malloc(row * c * sizeof(float));
+	memcpy(Wt, W, row * c * sizeof(float));
+	tran(Wt, row, c);
+
+	/* Project P = W'*Y */
+	mul(Wt, Y, P, c, row, column);
 
 	/* Free */
 	free(Y);
-	free(Z);
+	free(Wt);
 }
 
-static void center_data(float X[], float mu[], size_t row, size_t column) {
-	size_t i, j;
-	float* X0 = X;
-	float average;
-	/* Column-Major */
-	tran(X, row, column);
-	for (i = 0; i < column; i++) {
-		/* Average data mu[i] = mean(X) */
-		average = mean(X, row);
-
-		/* Center the data X = X - mu */
-		for (j = 0; j < row; j++) {
-			X[j] -= average;
-		}
-
-		/* Save the average */
-		mu[i] = average;
-
-		/* Jump to next row */
-		X += row;
-	}
-	X = X0;
-
-	/* Turn it back to Row-Major */
-	tran(X, column, row);
-}
-
-static void compute_components(float X[], float W[], size_t components, size_t row, size_t column) {
+static void compute_components(float X[], float W[], size_t c, size_t row, size_t column) {
 	/* Compute [U, S, V] = svd(A) */
 	float* U = (float*)malloc(row * column * sizeof(float));
 	float* S = (float*)malloc(column * sizeof(float));
 	float* V = (float*)malloc(column * column * sizeof(float));
-	float* V0 = V;
-	svd_jacobi_one_sided(X, column, U, S, V);
+	float* U0 = U;
+	if (row == column) {
+		svd_jacobi_one_sided(X, row, U, S, V);
+	}
+	else {
+		svd_golub_reinsch(X, row, column, U, S, V);
+	}
 
 	/* Get the components from V */
-	size_t i, bytes_shift = components * sizeof(float);
-	for (i = 0; i < column; i++) {
-		memcpy(W, V, bytes_shift); /* Move data from V to W, then shift the array position */
-		W += components;
-		V += column;
+	size_t i, bytes_shift = c * sizeof(float);
+	for (i = 0; i < row; i++) {
+		memcpy(W, U, bytes_shift); /* Move data from U to W, then shift the array position */
+		W += c;
+		U += column;
 	}
 
 	/* Reset */
-	V = V0;
+	U = U0;
 
 	/* Free */
 	free(U);
@@ -122,19 +100,16 @@ static void compute_components(float X[], float W[], size_t components, size_t r
 	  end
 
 	  % Average
-	  mu = mean(X);
+	  mu = mean(X); %cluster_filter(X)
 
 	  % Center data
-	  Z = X - mu;
-
-	  % Create the covariance
-	  Y = cov(Z);
+	  Y = X - mu;
 
 	  % PCA analysis
-	  [~, ~, V] = svd(Y, 0);
+	  [U, ~, ~] = svd(Y, 0);
 
 	  % Projection
-	  W = V(:, 1:c);
-	  P = Z*W;
+	  W = U(:, 1:c);
+	  P = W'*Y;
 	end
 */
