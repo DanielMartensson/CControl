597d049cc52ca535859e69cf8d307728fe591acd
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-07-30 15:56:38 +0200

    pdist2.c comment

--- src/CControl/Sources/Miscellaneous/pdist2.c
+++ src/CControl/Sources/Miscellaneous/pdist2.c
@@ -36,7 +36,7 @@ void pdist2(float A[], float B[], float C[], size_t row_a, size_t column_a, size
 		sum(AtimesA, AA, row_a, column_a, false);
 		
 		/* 
-		 * MATLAB: BB = sum(Bt*Bt, 1);
+		 * MATLAB: BB = sum(Bt.*Bt, 1);
 		 */
 		float* BttimesBt = (float*)malloc(sizeBytesB);
 		for (i = 0; i < row_b * column_a; i++) {
@@ -63,7 +63,7 @@ void pdist2(float A[], float B[], float C[], size_t row_a, size_t column_a, size
 		free(BB);
 		break;
 	}
-	case PDIST2_METRIC_L2: {
+	case PDIST2_METRIC_EUCLIDEAN: {
 		pdist2(A, B, C, row_a, column_a, row_b, PDIST2_METRIC_SQEUCLIDEAN);
 		for (i = 0; i < row_a * row_b; i++) {
 			C[i] = sqrtf(C[i]);
@@ -72,3 +72,75 @@ void pdist2(float A[], float B[], float C[], size_t row_a, size_t column_a, size
 	}
 	}
 }
+
+/* GNU Octave code:
+ *  
+ * 
+	 switch metric
+	  case {0,'sqeuclidean'}
+		D = distEucSq( X, Y );
+	  case 'euclidean'
+		D = sqrt(distEucSq( X, Y ));
+	  case 'L1'
+		D = distL1( X, Y );
+	  case 'cosine'
+		D = distCosine( X, Y );
+	  case 'emd'
+		D = distEmd( X, Y );
+	  case 'chisq'
+		D = distChiSq( X, Y );
+	  otherwise
+		error(['pdist2 - unknown metric: ' metric]);
+	end
+	D = max(0,D);
+	end
+
+	function D = distL1( X, Y )
+		m = size(X,1);  n = size(Y,1);
+		mOnes = ones(1,m); D = zeros(m,n);
+		for i=1:n
+		  yi = Y(i,:);  yi = yi( mOnes, : );
+		  D(:,i) = sum( abs( X-yi),2 );
+		end
+	end
+
+	function D = distCosine( X, Y )
+		p=size(X,2);
+		XX = sqrt(sum(X.*X,2)); X = X ./ XX(:,ones(1,p));
+		YY = sqrt(sum(Y.*Y,2)); Y = Y ./ YY(:,ones(1,p));
+		D = 1 - X*Y';
+	end
+
+	function D = distEmd( X, Y )
+		Xcdf = cumsum(X,2);
+		Ycdf = cumsum(Y,2);
+		m = size(X,1);  n = size(Y,1);
+		mOnes = ones(1,m); D = zeros(m,n);
+		for i=1:n
+		  ycdf = Ycdf(i,:);
+		  ycdfRep = ycdf( mOnes, : );
+		  D(:,i) = sum(abs(Xcdf - ycdfRep),2);
+		end
+	end
+
+	function D = distChiSq( X, Y )
+		% note: supposedly it's possible to implement this without a loop!
+		m = size(X,1);  n = size(Y,1);
+		mOnes = ones(1,m); D = zeros(m,n);
+		for i=1:n
+		  yi = Y(i,:);  yiRep = yi( mOnes, : );
+		  s = yiRep + X;    d = yiRep - X;
+		  D(:,i) = sum( d.^2 ./ (s+eps), 2 );
+		end
+		D = D/2;
+	end
+ 
+
+	function D = distEucSq(X, Y)
+	  Yt = Y';
+	  XX = sum(X.*X,2);
+	  YY = sum(Yt.*Yt,1);
+	  D = bsxfun(@plus,XX,YY)-2*X*Yt;
+	end
+
+*/
