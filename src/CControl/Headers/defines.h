fc44601a4398b7764bc742cb3ba9d8104803f4ae
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-02 19:44:33 +0200

    Move bool true and move bool false

--- src/CControl/Headers/defines.h
+++ src/CControl/Headers/defines.h
@@ -14,11 +14,6 @@
 #endif
 
 #if __STDC_VERSION__ < 199901L
-#define true 1
-#define false 0
-#endif /* !__STDC_VERSION___ */
-
-#if __STDC_VERSION__ < 199901L
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
@@ -26,6 +21,8 @@ typedef signed char int8_t;
 typedef signed short int16_t;
 typedef signed int int32_t;
 typedef uint8_t bool;
+#define true 1
+#define false 0
 #ifndef _MSC_VER
 #define NULL ((void *)0)
 typedef unsigned long size_t;
