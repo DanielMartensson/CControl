0d40f309c63b344d0ea6546e6160f96e139653c7
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-10 08:20:05 +0200

    Faster tran.c

--- src/CControl/Sources/LinearAlgebra/tran.c
+++ src/CControl/Sources/LinearAlgebra/tran.c
@@ -7,39 +7,10 @@
 
 #include "../../Headers/functions.h"
 
- /* Include LAPACK routines */
-#ifdef CLAPACK_USED
-#include "CLapack/clapack_functions.h"
-#endif
-
 /*
  * Turn A into transponse A^T
  */
 void tran(float A[], size_t row, size_t column) {
-#ifdef MKL_USED
-	/* Create identy matrix */
-	float* I = (float*)malloc(row * row * sizeof(float));
-	memset(I, 0, row * row * sizeof(float));
-	float* I0 = I;
-	size_t i;
-	for (i = 0; i < row; i++) {
-		I0[i] = 1.0f;
-		I0 += row;
-	}
-
-	/* Create output matrix */
-	float* At = (float*)malloc(column * row * sizeof(float));
-
-	/* Compute At = alpha*A'*I + beta*At */
-	cblas_sgemm(CblasRowMajor, CblasTrans, CblasNoTrans, column, row, row, 1.0f, A, column, I, row, 0.0f, At, row);
-
-	/* Copy over */
-	memcpy(A, At, column * row * sizeof(float));
-
-	/* Free */
-	free(I);
-	free(At);
-#else
 	/* Decleration */
 	float *B = (float*)malloc(row * column * sizeof(float));
 	float* transpose = NULL;
@@ -60,7 +31,6 @@ void tran(float A[], size_t row, size_t column) {
 
 	/* Free */
 	free(B);
-#endif
 }
 
 /*
