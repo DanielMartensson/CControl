9025540a5c47e82e32610655f09c17bcde22f9f9
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-08-13 14:56:56 +0200

    tolerance to FLT_EPSILON

--- src/CControl/Sources/MachineLearning/svm.c
+++ src/CControl/Sources/MachineLearning/svm.c
@@ -96,7 +96,7 @@ bool svm(float X[], float y[], float w[], float* b, float* accuracy, float C, fl
 
 	/* Find weights w = (alpha.*y')*X */
 	float* X0 = X;
-	const float tol = 1e-5f;
+	const float tol = FLT_EPSILON;
 	for (j = 0; j < column; j++) {
 		/* Support vectors have non zero lagrange multipliers */
 		X = X0;
