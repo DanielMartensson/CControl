5aa54f2c31e7bf28bd1d861cb35e274ab98123be
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-09-03 21:40:18 +0200

    C++ compability for FFTPack 5.1

--- src/CControl/Sources/SignalProcessing/FFTpack_5_1/fftpack.h
+++ src/CControl/Sources/SignalProcessing/FFTpack_5_1/fftpack.h
@@ -1,9 +1,17 @@
 #ifndef FFTPACK
 #define FFTPACK
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "f2c.h"
 int rfft2i_(integer* l, integer* m, real* wsave, integer* lensav, integer* ier);
 int rfft2f_(integer* ldim, integer* l, integer* m, real* r__, real* wsave, integer* lensav, real* work, integer* lenwrk, integer* ier);
 int rfft2b_(integer* ldim, integer* l, integer* m, real* r__, real* wsave, integer* lensav, real* work, integer* lenwrk, integer* ier);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* FFTPACK */
