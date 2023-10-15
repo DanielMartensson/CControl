f9d39a07e9216a4452b610ee08f4dbe748d15bf2
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-10-15 17:53:11 +0200

    Update main.c

--- src/Main.c
+++ src/Main.c
@@ -1,42 +1,26 @@
 /*
  ============================================================================
- Name        : lbp.c
+ Name        : Main.c
  Author      : <Your Name Here>
  Version     : 1.0
  Copyright   : MIT
- Description : Find a descriptor from Local Binary Pattern
+ Description : Initial template
  ============================================================================
  */
 
 #include "CControl/ccontrol.h"
-
-#define row 20
-#define column 20
 
 int main() {
 	clock_t start, end;
 	float cpu_time_used;
 	start = clock();
 
-	/* Create X */
-	float X[row * column];
-
-	/* Random */
-	randn(X, row * column, 0, 1);
-
-	/* Local binary pattern */
-	uint64_t descriptor = lbp(X, row, column, 0.0f, 3, LBP_BIT_16);
-
-	/* Print descriptor */
-	size_t i;
-	printf("0b");
-	for (i = 0; i < 16; i++) {
-		printf("%i", 0b1 & (descriptor >> i));
-	}
-	printf("\n");
+	/* Your ANSI C logic here */
 
 	end = clock();
 	cpu_time_used = ((float)(end - start)) / CLOCKS_PER_SEC;
 	printf("\nTotal speed  was %f\n", cpu_time_used);
+
+
 	return EXIT_SUCCESS;
 }
