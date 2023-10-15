200a3aeb4debce1500fb7b8b206d7b71a2272e27
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-10-15 17:31:47 +0200

    Update example

--- src/CControl/Sources/ImageProcessing/lbp.c
+++ src/CControl/Sources/ImageProcessing/lbp.c
@@ -75,48 +75,63 @@ uint64_t lbp(const float X[], const size_t row, const size_t column, const float
 }
 
 /* C-code for drawing the circle
-#include <stdio.h>
-#include <math.h>
-#include <stdint.h>
+
+#include "CControl/ccontrol.h"
 
 void drawCircle(const const int M, const int N, const uint8_t radius, char matrix[], const float init_angle, const float angles[], const uint8_t total_angles) {
-	size_t centerX = M / 2.0;
-	size_t centerY = N / 2.0;
+	size_t centerX = M / 2;
+	size_t centerY = N / 2;
 
 	for (int i = 0; i < total_angles; i++) {
 		float angle = init_angle + angles[i];
 		int x = roundf(centerX + radius * cosf(angle));
 		int y = roundf(centerY + radius * sinf(angle));
-		matrix[x*N + y] = '.';
+		matrix[x * N + y] = '.';
 	}
 }
 
 void printMatrix(int M, int N, char matrix[]) {
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
-			printf("%c ", matrix[i*N + j]);
+			printf("%c ", matrix[i * N + j]);
 		}
 		printf("\n");
 	}
 }
 
 int main() {
+	// Size of the matrix
 	int M = 40;
 	int N = 40;
+	char* matrix = (char*)malloc(M * N);
+
+	// Change this
 	float radius = 10;
 	int init_angle = 0;
-	char matrix[M*N];
-	const float steps8[8] = {0.0f, 0.7854f, 1.5708f, 2.3562f, 3.1416f, 3.927f, 4.7124f, 5.4978f};
-	const float steps16[16] = {0.0f, 0.3927f, 0.7854f, 1.1781f, 1.5708f, 1.9635f, 2.3562f, 2.7489f, 3.1416f, 3.5343f, 3.927f, 4.3197f, 4.7124f, 5.1051f, 5.4978f, 5.8905f};
-	const float steps32[32] = {0.0f, 0.19635f, 0.3927f, 0.58905f, 0.7854f, 0.98175f, 1.1781f, 1.3744f, 1.5708f, 1.7671f, 1.9635f, 2.1598f, 2.3562f, 2.5525f, 2.7489f, 2.9452f, 3.1416f, 3.3379f, 3.5343f, 3.7306f, 3.927f, 4.1233f, 4.3197f, 4.516f, 4.7124f, 4.9087f, 5.1051f, 5.3014f, 5.4978f, 5.6941f, 5.8905f, 6.0868f};
-	const float steps64[64] = {0.0f, 0.098175f, 0.19635f, 0.29452f, 0.3927f, 0.49087f, 0.58905f, 0.68722f, 0.7854f, 0.88357f, 0.98175f, 1.0799f, 1.1781f, 1.2763f, 1.3744f, 1.4726f, 1.5708f, 1.669f, 1.7671f, 1.8653f, 1.9635f, 2.0617f, 2.1598f, 2.258f, 2.3562f, 2.4544f, 2.5525f, 2.6507f, 2.7489f, 2.8471f, 2.9452f, 3.0434f, 3.1416f, 3.2398f, 3.3379f, 3.4361f, 3.5343f, 3.6325f, 3.7306f, 3.8288f, 3.927f, 4.0252f, 4.1233f, 4.2215f, 4.3197f, 4.4179f, 4.516f, 4.6142f, 4.7124f, 4.8106f, 4.9087f, 5.0069f, 5.1051f, 5.2033f, 5.3014f, 5.3996f, 5.4978f, 5.596f, 5.6941f, 5.7923f, 5.8905f, 5.9887f, 6.0868f, 6.185f};
+
+	// Angles in radians
+	const float steps8[8] = { 0.0f, 0.7854f, 1.5708f, 2.3562f, 3.1416f, 3.927f, 4.7124f, 5.4978f };
+	const float steps16[16] = { 0.0f, 0.3927f, 0.7854f, 1.1781f, 1.5708f, 1.9635f, 2.3562f, 2.7489f, 3.1416f, 3.5343f, 3.927f, 4.3197f, 4.7124f, 5.1051f, 5.4978f, 5.8905f };
+	const float steps32[32] = { 0.0f, 0.19635f, 0.3927f, 0.58905f, 0.7854f, 0.98175f, 1.1781f, 1.3744f, 1.5708f, 1.7671f, 1.9635f, 2.1598f, 2.3562f, 2.5525f, 2.7489f, 2.9452f, 3.1416f, 3.3379f, 3.5343f, 3.7306f, 3.927f, 4.1233f, 4.3197f, 4.516f, 4.7124f, 4.9087f, 5.1051f, 5.3014f, 5.4978f, 5.6941f, 5.8905f, 6.0868f };
+	const float steps64[64] = { 0.0f, 0.098175f, 0.19635f, 0.29452f, 0.3927f, 0.49087f, 0.58905f, 0.68722f, 0.7854f, 0.88357f, 0.98175f, 1.0799f, 1.1781f, 1.2763f, 1.3744f, 1.4726f, 1.5708f, 1.669f, 1.7671f, 1.8653f, 1.9635f, 2.0617f, 2.1598f, 2.258f, 2.3562f, 2.4544f, 2.5525f, 2.6507f, 2.7489f, 2.8471f, 2.9452f, 3.0434f, 3.1416f, 3.2398f, 3.3379f, 3.4361f, 3.5343f, 3.6325f, 3.7306f, 3.8288f, 3.927f, 4.0252f, 4.1233f, 4.2215f, 4.3197f, 4.4179f, 4.516f, 4.6142f, 4.7124f, 4.8106f, 4.9087f, 5.0069f, 5.1051f, 5.2033f, 5.3014f, 5.3996f, 5.4978f, 5.596f, 5.6941f, 5.7923f, 5.8905f, 5.9887f, 6.0868f, 6.185f };
+
+	// Fill the matrix
 	for (int i = 0; i < M; i++) {
 		for (int j = 0; j < N; j++) {
-			matrix[i*N + j] = ' ';
+			matrix[i * N + j] = ' ';
 		}
 	}
-	drawCircle(M, N, radius, matrix, init_angle, steps64, 64);
+
+	// Draw a circle
+	drawCircle(M, N, radius, matrix, init_angle * PI / 180.0f, steps64, 64);
+
+	// Print a circle
 	printMatrix(M, N, matrix);
+
+	// Free
+	free(matrix);
+
+	// Return zero
 	return 0;
 }
 */
