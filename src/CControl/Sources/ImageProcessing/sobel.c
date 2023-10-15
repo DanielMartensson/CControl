ffa6f79721ed9573a547d1e3732585685614f766
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-10-15 12:08:25 +0200

    Change to radians

--- src/CControl/Sources/ImageProcessing/sobel.c
+++ src/CControl/Sources/ImageProcessing/sobel.c
@@ -11,7 +11,7 @@
  * Compute sobel filter
  * X[m*n] - Image
  * G[m*n] - Gradients with L2-norm
- * O[m*n] - Orientations in degrees
+ * O[m*n] - Orientations in radians
  */
 void sobel(float X[], float G[], float O[], size_t row, size_t column, bool only_compute_G) {
 	/* Create kernel matrix */
@@ -42,7 +42,7 @@ void sobel(float X[], float G[], float O[], size_t row, size_t column, bool only
 	else {
 		for (i = 0; i < total_size; i++) {
 			G[i] = sqrtf(Gx[i] * Gx[i] + Gy[i] * Gy[i]);
-			O[i] = atan2f(Gy[i], Gx[i]) * 180.0f / PI;
+			O[i] = atan2f(Gy[i], Gx[i]);
 		}
 	}
 
