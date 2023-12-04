0f49e1d57348e669469606ca721c1813ced5da89
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-12-04 21:11:49 +0100

    Add array min c-file

---
+++ src/CControl/Sources/Statistics/amin.c
@@ -0,0 +1,30 @@
+/*
+ * amin.c
+ *
+ *  Created on: 4 december 2023
+ *      Author: Daniel Mårtensson
+ */
+
+#include "../../Headers/functions.h"
+
+ /*
+  * Compute the min value of vector x
+  * x[L] Vector with values
+  * min_index is the minimum index of the minimum value
+  * L = Length of vector x
+  */
+float amin(const float x[], size_t* min_index, const size_t length) {
+	/* Default settings */
+	float min_value = x[0];
+	*min_index = 0;
+
+	/* Find if the default setting was not true */
+	size_t i;
+	for (i = 1; i < length; i++) {
+		if (x[i] < min_value) {
+			min_value = x[i];
+			*min_index = i;
+		}
+	}
+	return min_value;
+}
