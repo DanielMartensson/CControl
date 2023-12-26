c59ac02754a2251a69156e43eb6b1b9e38e8b815
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-12-26 14:35:03 +0100

    Add ind2sub.c. The same as Matlab's ind2sub.m

---
+++ src/CControl/Sources/Miscellaneous/ind2sub.c
@@ -0,0 +1,16 @@
+/*
+ * ind2sub.c
+ *
+ *  Created on: December 23, 2023
+ *      Author: Daniel Mårtensson
+ */
+
+#include "../../Headers/functions.h"
+
+/*
+ * This is the MATLAB function ind2sub for generate the coordinate of an index at a matrix
+ */
+void ind2sub(const size_t index, const size_t column, size_t* row_index, size_t* column_index) {
+    *row_index = index / column;
+    *column_index = index % column;
+}
