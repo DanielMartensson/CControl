6f5baec8699fa8421cdd1f8d17e2d37e98dd4c93
Daniel Mårtensson <daniel.martensson100@outlook.com>
2023-12-29 01:40:03 +0100

    Optimized transpose

--- src/CControl/Sources/LinearAlgebra/tran.c
+++ src/CControl/Sources/LinearAlgebra/tran.c
@@ -11,28 +11,45 @@
  * Turn A into transponse A^T
  * A[m*n]
  */
-void tran(float A[], size_t row, size_t column) {
-	/* Decleration */
-	float *B = (float*)malloc(row * column * sizeof(float));
-	float* transpose = NULL;
-	float *ptr_A = A;
+void tran(float A[], const size_t row, const size_t column) {
+	/* Declare */
+	float* B = (float*)malloc(row * column * sizeof(float));
 	size_t i, j;
-
 	for (i = 0; i < row; i++) {
-		transpose = &B[i];
 		for (j = 0; j < column; j++) {
-			*transpose = *ptr_A;
-			ptr_A++;
-			transpose += row;
+			B[j * row + i] = A[i * column + j];
 		}
 	}
 
-	/* Copy! */
-	memcpy(A, B, row*column*sizeof(float));
+	/* Copy over */
+	memcpy(A, B, row * column * sizeof(float));
 
 	/* Free */
 	free(B);
 }
+
+/* Old code
+// Decleration
+float* B = (float*)malloc(row * column * sizeof(float));
+float* transpose = NULL;
+float* ptr_A = A;
+size_t i, j;
+
+for (i = 0; i < row; i++) {
+	transpose = &B[i];
+	for (j = 0; j < column; j++) {
+		*transpose = *ptr_A;
+		ptr_A++;
+		transpose += row;
+	}
+}
+
+// Copy!
+memcpy(A, B, row * column * sizeof(float));
+
+// Free
+free(B);
+*/
 
 /*
  * GNU Octave code:
