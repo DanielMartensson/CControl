fe250706a96e7a4c6269da5ae5af0f40f8aa17fc
Daniel Mårtensson <daniel.martensson100@outlook.com>
2024-01-14 22:25:45 +0100

    Create Normal Propability Densitity function with inline

---
+++ src/CControl/Sources/Statistics/normpdf.c
@@ -0,0 +1,15 @@
+/*
+ * normpdf.c
+ *
+ *  Created on: 10 januari 2024
+ *      Author: Daniel Mårtensson
+ */
+
+#include "statistics.h"
+
+/*
+ * Compute the normal propability density function
+ */
+INLINE float normpdf(float x, float mu, float sigma) {
+	return 1.0f / (sigma * sqrtf(2.0f * PI)) * expf(-1.0f / 2.0f * (x - mu) * (x - mu) / (sigma * sigma));
+}
