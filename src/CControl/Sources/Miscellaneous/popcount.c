e79c5ea4c929a6f2a52a7c78879fa67bf49385ef
Daniel Mårtensson <daniel.martensson100@outlook.com>
2024-01-14 22:24:13 +0100

    Add built in popcount

--- src/CControl/Sources/Miscellaneous/popcount.c
+++ src/CControl/Sources/Miscellaneous/popcount.c
@@ -10,12 +10,18 @@
 /*
  * Count the amount of 1:s inside a binary number
  */
-uint8_t popcount(const uint64_t x) {
-    uint64_t y = x;
+INLINE uint8_t popcount(const uint32_t x) {
+#ifdef _MSC_VER
+    return __popcnt(x);
+#elif defined(__GNUC__)
+    return __builtin_popcount(x);
+#else
+    uint32_t y = x;
     uint8_t count = 0U;
     while (y) {
         count += y & 1U;
         y >>= 1U;
     }
     return count;
+#endif /* _MSC_VER */
 }
