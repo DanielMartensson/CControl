e2e357a6bb14b2c4a7e33fb29e63fb3bcdfbe195
Daniel Mårtensson <daniel.martensson100@outlook.com>
2024-07-30 15:21:05 +0200

    Give MPC soft constraints on output

--- src/CControl/Sources/ControlEngineering/controlengineering.h
+++ src/CControl/Sources/ControlEngineering/controlengineering.h
@@ -10,10 +10,6 @@ extern "C" {
 void c2d(float A[], float B[], const size_t row_a, const size_t column_b, const float sampleTime);
 void cab(float GAMMA[], const float PHI[], const float B[], const float C[], const size_t row_a, const size_t row_c, const size_t column_b, const size_t N);
 void qmpc(const float GAMMA[], const float PHI[], const float x[], float u[], const float Umax[], const float Ymax[], const float r[], const size_t row_a, const size_t row_c, const size_t column_b, const size_t N, const float lambda, const bool has_integration_action, const float integration_constant);
-void kalman(float A[], float B[], float C[], float K[], float u[], float x[], float y[], size_t ADIM, size_t YDIM, size_t RDIM);
-void lqi(float y[], float u[], float qi, float r[], float L[], float Li[], float x[], float xi[], size_t ADIM, size_t YDIM, size_t RDIM, ANTI_WINUP anti_winup);
-void mrac(float limit, float gain, float y[], float u[], float r[], float I1[], float I2[], size_t RDIM);
-void theta2ss(float A[], float B[], float C[], float theta[], float K[], size_t ADIM, size_t NP, size_t NZ, bool integral_action);
 void obsv(float PHI[], const float A[], const float C[], const size_t row_a, const size_t row_c, const size_t N);
 bool ssint(const float A[], const float B[], const float C[], float Ai[], float Bi[], float Ci[], const size_t row_a, const size_t column_b, const size_t row_c);
 bool stability(const float A[], const size_t row);
