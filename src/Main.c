1090ab54c86c68eab747ed8ea051d2fb8353775a
Daniel Martensson <daniel.martensson100@outlook.com>
2025-01-29 12:30:51 +0100

    Update working example - Need to simulate dare.c

--- src/Main.c
+++ src/Main.c
@@ -1,122 +1,159 @@
 /*
  ============================================================================
- Name        : qmpc.c
- Author      : <Your Name Here>
+ Name        : main.c
+ Author      : Daniel Mårtensson
  Version     : 1.0
  Copyright   : MIT
- Description : Model Predictive Control with integral action and constraints on inputs and outputs
+ Description : Simple test
  ============================================================================
  */
 
 #include "CControl/ccontrol.h"
 
-#define row_a 2
-#define column_b 1
-#define row_c 1
-#define row_ai (row_a + column_b)
-#define column_bi column_b
-#define row_ci row_c
-#define N 10
-#define SAMPLE_TIME 0.5f
-#define SETPOINT 6.0f
-#define LAMBDA 0.2f
-#define MAX_U 0.4f
-#define INTEGRATION_CONSTANT 0.2f
-#define HAS_INTEGRATION_ACTION true
+ /* Constants */
+#define row_A 2
+#define column_B 2
+
+/*
+ * Compute R'*Y*U = I
+ * R[row_r * column_r]
+ * Y[row_y * column_y]
+ * U[row_u * column_u]
+ * I[column_r * column_u]
+ */
+static void RTYUI(const float R[], const float Y[], const float U[], float I[], const size_t row_r, const size_t column_r, const size_t row_y, const size_t column_y, const size_t row_u, const size_t column_u) {
+	/* Turn R into transpose */
+	float* RT = (float*)malloc(row_r * column_r * sizeof(float));
+	memcpy(RT, R, row_r * column_r * sizeof(float));
+	tran(RT, row_r, column_r);
+
+	/* Compute YU = Y*U */
+	float* YU = (float*)malloc(row_y * column_u * sizeof(float));
+	mul(Y, U, YU, row_y, column_y, column_u);
+
+	/* Compute I = RT*YU */
+	mul(RT, YU, I, column_r, row_r, column_u);
+
+	/* Free */
+	free(RT);
+	free(YU);
+}
+
+/* Discrete Algebraic Riccati Equation
+ * Iterate: X = A'*X*A - X - A'*X*B*(B'*X*B + R)^{-1}*B'*X*A + Q
+ */
+static void odefun(const float t, float X[], const float* matrices[], const size_t rows[], const size_t columns[]) {
+	/* Get the matrices */
+	const float* A = matrices[0];
+	const float* B = matrices[1];
+	const float* Q = matrices[2];
+	const float* R = matrices[3];
+
+	/* row_a and column_a has the same value */
+	const size_t row_a = rows[0];
+	const size_t column_a = columns[0];
+
+	/* row_b has the same value as row_a */
+	const size_t row_b = rows[1];
+	const size_t column_b = columns[1];
+
+	/* row_q and column_q has the same value as row_a and column_a */
+	const size_t row_q = rows[2];
+	const size_t column_q = columns[2];
+
+	/* row_r and column_r has the same value as column_b */
+	const size_t row_r = rows[3];
+	const size_t column_r = columns[3];
+
+	/* row_x and column_x has the same value as row_a and column_a */
+	const size_t row_x = row_a;
+	const size_t column_x = column_a;
+
+	/* Compute A'*X*A = ATXA */
+	float* ATXA = (float*)malloc(column_a * column_a * sizeof(float));
+	RTYUI(A, X, A, ATXA, row_a, column_a, row_x, column_x, row_a, column_a);
+
+	/* Compute A'*X*B = ATXB */
+	float* ATXB = (float*)malloc(column_a * column_b * sizeof(float));
+	RTYUI(A, X, B, ATXB, row_a, column_a, row_x, column_x, row_b, column_b);
+
+	/* Compute B'*X*B + R = BTXBpR */
+	float* BTXBpR = (float*)malloc(column_b * column_b * sizeof(float));
+	RTYUI(B, X, B, BTXBpR, row_b, column_b, row_x, column_x, row_b, column_b);
+	size_t i;
+	for (i = 0; i < column_b * column_b; i++) {
+		BTXBpR[i] = R[i];
+	}
+
+	/* Do inverse of BTXBpR */
+	inv(BTXBpR, column_b);
+
+	/* Compute B'*X*A = BTXA */
+	float* BTXA = (float*)malloc(column_b * column_a * sizeof(float));
+	RTYUI(B, X, A, BTXA, row_b, column_b, row_x, column_x, row_a, column_a);
+
+	/* Compute ATXB * BTXBpR * BTXA = ATXB_BTXBpR_BTXA */
+	float* BTXBpR_BTXA = (float*)malloc(column_b * column_a * sizeof(float));
+	mul(BTXBpR, BTXA, BTXBpR_BTXA, column_b, column_b, column_a);
+	float* ATXB_BTXBpR_BTXA = (float*)malloc(column_a * column_a * sizeof(float));
+	mul(ATXB, BTXBpR_BTXA, ATXB_BTXBpR_BTXA, column_a, column_b, column_a);
+
+	/* Return X */
+	for (i = 0; i < row_x * column_x; i++) {
+		X[i] = ATXA[i] - X[i] - ATXB_BTXBpR_BTXA[i] + Q[i];
+	}
+
+	/* Free */
+	free(ATXA);
+	free(ATXB);
+	free(BTXBpR);
+	free(BTXA);
+	free(BTXBpR_BTXA);
+	free(ATXB_BTXBpR_BTXA);
+}
 
 int main() {
-
-    /* Create A matrix */
-    float A[row_a * row_a] = { 0, 1,
-                              -1, -1 };
-
-    /* Create B matrix */
-    float B[row_a * column_b] = { 0,
-                                  1 };
-
-    /* Create C matrix */
-    float C[row_c * row_a] = { 1, 0};
-
-    /* Turn the SS model into a discrete SS model */
-    c2d(A, B, row_a, column_b, SAMPLE_TIME);
-
-    /* Create Ai matrix */
-    float Ai[row_ai * row_ai];
-
-    /* Create Bi matrix */
-    float Bi[row_ai * column_bi];
-
-    /* Create Ci matrix */
-    float Ci[row_ci * row_ai];
-
-    /* Add integral action */
-    ssint(A, B, C, Ai, Bi, Ci, row_a, column_b, row_c);
-
-    /* Create PHI matrix */
-    float PHI[(N * row_ci) * row_ai];
-    obsv(PHI, Ai, Ci, row_ai, row_ci, N);
-
-    /* Create GAMMA matrix */
-    float GAMMA[(N * row_ci) * (N * column_bi)];
-    cab(GAMMA, PHI, Bi, Ci, row_ai, row_ci, column_bi, N);
-
-    /* Create vectors: state vector x, input signal u, reference vector r, maximum output signal Umax, slack variable values S */
-    float x[row_ai], u[column_bi], r[row_ci], Umax[column_bi], S[row_ci];
-    u[0] = 0;
-    r[0] = SETPOINT;
-    S[0] = 2.0f;
-    Umax[0] = MAX_U;
-    x[0] = -8;
-    x[1] = 20;
-    x[2] = 0;
-
     clock_t start, end;
     float cpu_time_used;
     start = clock();
 
-    /* This function should be placed inside a while-loop inside a microcontroller */
-    qmpc(GAMMA, PHI, x, u, Umax, S, r, row_ai, row_ci, column_bi, N, LAMBDA, HAS_INTEGRATION_ACTION, INTEGRATION_CONSTANT);
+	const float A[row_A * row_A] = { 1, 2, 3, 4 };
+	const float B[row_A * column_B] = { -2, 1, 4, 0 };
+	const float Q[row_A * row_A] = { 1, 0, 2, 3 };
+	const float R[column_B * column_B] = { -1, -2, -3, 4 };
+	float X[row_A * row_A] = {0, 1, -2, 1 };
+
+	const float* matrices[4] = { A, B, Q, R };
+	const size_t rows[4] = { row_A, row_A, row_A, column_B };
+	const size_t columns[4] = { row_A, column_B, row_A, column_B };
+
+	odefun(0, X, matrices, rows, columns);
+
+	printf("This is X:\n");
+	print(X, row_A, row_A);
+
 
     end = clock();
     cpu_time_used = ((float)(end - start)) / CLOCKS_PER_SEC;
     printf("\nTotal speed  was %f\n", cpu_time_used);
 
-    /* Print the output signal */
-    print(u, column_bi, 1);
 
-    /* Detect memory leak */
+    /* Check memory */
     detectmemoryleak();
 
     return EXIT_SUCCESS;
 }
 
-/* GNU octave code:
-% You need to install MataveControl from GitHub https://github.com/DanielMartensson/MataveControl/
+/*
+ GNU Octave code:
 
-close all
-clear all
-clc
 
-sys = mc.ss(0, [0 1; -1 -1], [0;1], [1 0]); % SISO state space model
+A = [ 1, 2; 3, 4 ];
+B = [ -2, 1; 4, 0 ];
+Q = [ 1, 0; 2, 3 ];
+R = [ -1, -2; -3, 4 ];
+X = [0, 1; -2, 1 ];
 
-sysd = mc.c2d(sys, 0.5); % To discrete
+X = A'*X*A - X - A'*X*B*inv(B'*X*B + R)*B'*X*A + Q
 
-R = [6]; % Reference for the SISO model. If MIMO -> R need to be a vector
-N = 10; % Horizon predict constant
-T = 35; % Horizon time constant
-lambda = 7; % Regularization for smoother inputs u
-
-[y, t, x, u] = mc.lmpc(sysd, N, R, T, lambda); % Simulate MPC with linear programming
-hold on
-plot(t, u)
-
-I = 0.2; % Integral action constant
-Umax = [0.4]; % Maximum input signal vector
-S = [2]; % Slack variable says that the output can be +2 over the reference R, in this case: 6+2 = 8
-lambda = 0.2; % Regularization for smoother inputs u
-figure(2); % New figure
-x0 = [-8; 20];
-[y, t, x, u] = mc.qmpc(sysd, N, R, T, lambda, Umax, S, I, x0); % Simulate MPC with quadratic programming
-hold on
-plot(t, u)
 */
