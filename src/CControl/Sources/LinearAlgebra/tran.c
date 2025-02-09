8bb1b3626d583cc7077c9561ba76c5af01567bce
Daniel Martensson <daniel.martensson100@outlook.com>
2025-02-09 23:01:22 +0100

    Optimized transpose

--- src/CControl/Sources/LinearAlgebra/tran.c
+++ src/CControl/Sources/LinearAlgebra/tran.c
@@ -12,39 +12,37 @@
  * A[m*n]
  */
 void tran(float A[], const size_t row, const size_t column) {
-	/* Declare */
-	float* B = (float*)malloc(row * column * sizeof(float));
-	size_t i, j;
-	for (i = 0; i < row; i++) {
-		for (j = 0; j < column; j++) {
-			B[j * row + i] = A[i * column + j];
+	size_t start, next, i;
+	float tmp;
+	for (start = 0; start <= column * row - 1; start++) {
+		next = start;
+		i = 0;
+		do {
+			i++;
+			next = (next % row) * column + next / row;
+		} while (next > start);
+		if (next < start || i == 1) {
+			continue;
 		}
+		tmp = A[next = start];
+		do {
+			i = (next % row) * column + next / row;
+			A[next] = (i == start) ? tmp : A[i];
+			next = i;
+		} while (next > start);
 	}
-
-	/* Copy over */
-	memcpy(A, B, row * column * sizeof(float));
-
-	/* Free */
-	free(B);
 }
 
 /* Old code
-// Decleration
 float* B = (float*)malloc(row * column * sizeof(float));
-float* transpose = NULL;
-float* ptr_A = A;
 size_t i, j;
-
 for (i = 0; i < row; i++) {
-	transpose = &B[i];
 	for (j = 0; j < column; j++) {
-		*transpose = *ptr_A;
-		ptr_A++;
-		transpose += row;
+		B[j * row + i] = A[i * column + j];
 	}
 }
 
-// Copy!
+// Copy over 
 memcpy(A, B, row * column * sizeof(float));
 
 // Free
