9d9a69766e56324139b1fedeb60b5a5db01dd812
Daniel Mårtensson <daniel.martensson100@outlook.com>
2025-02-14 09:59:01 +0100

    Update mpc.c - replace quadprog with quadprogslim

--- src/CControl/Sources/ControlEngineering/mpc.c
+++ src/CControl/Sources/ControlEngineering/mpc.c
@@ -1024,7 +1024,7 @@ bool mpc_optimize(MPC* mpc, float u[], const float r[], const float y[], const f
 
 	/* Quadraptic programming output */
 	float* U = (float*)malloc(2 * N * column_b * sizeof(float));
-	const bool status = quadprog(mpc->barH, barg, aqp, bqp, NULL, NULL, U, 2 * ((N - 1) * column_b + 2 * N * row_c) + 2 * (N * column_b + N), 0, 2 * N * column_b, false);
+	const bool status = quadprogslim(mpc->barH, barg, aqp, bqp, NULL, NULL, U, 2 * ((N - 1) * column_b + 2 * N * row_c) + 2 * (N * column_b + N), 0, 2 * N * column_b, false);
 
 	/* Get amount of output from U */
 	memcpy(u, U, column_b * sizeof(float));
