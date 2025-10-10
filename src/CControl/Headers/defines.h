8572575b261ac1caf954dfd9afbf4a9d8fd2acc6
Daniel Martensson <daniel.martensson100@outlook.com>
2025-10-10 12:55:45 +0200

    Ifndef

--- src/CControl/Headers/defines.h
+++ src/CControl/Headers/defines.h
@@ -53,11 +53,11 @@ typedef unsigned long long size_t;
 #define CONV_MAX_KERNEL_FFT_INSTEAD 80  /* When we are going to use FFT with conv or conv2 */
 
 /* Select library by uncomment - If non of these are uncomment, then CControl will use the internal library instead */
-#ifdef ARM_IS_USED
+#ifndef ARM_IS_USED
 #define MKL_LAPACK_USED 				/* For large matrices on a regular computer */
 #define MKL_FFT_USED					/* For large matrices on a regular computer */
 #else
-#define CLAPACK_USED  					/* For larger embedded systems */
+#define CLAPACK_USED1  					/* For larger embedded systems */
 #endif /* !ARM_IS_USED */
 
 #endif /* !DEFINES_H_ */
