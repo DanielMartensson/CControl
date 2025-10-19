c19ed2aa20f0939cfb4cee4cbe6c1ff51dac8487
Daniel Martensson <daniel.martensson100@outlook.com>
2025-10-19 22:48:57 +0200

    add functions

--- src/CControl/Headers/functions.h
+++ src/CControl/Headers/functions.h
@@ -12,8 +12,62 @@
 extern "C" {
 #endif
 
-/* Add functions here */
+#if __STDC_VERSION__ < 199901L
+#if !defined(_MSC_VER)
+float sqrtf(float x) {
+	return (float)sqrt(x);
+}
 
+float fabsf(float x) {
+	return (float)fabs(x);
+}
+
+float acosf(float x) {
+	return (float)acos(x);
+}
+
+float atan2f(float x) {
+	return (float)atan(x);
+}
+
+float expf(float x) {
+	return (float)exp(x);
+}
+
+float powf(float base, float power) {
+	return (float)pow(base, power);
+}
+
+float logf(float x) {
+	return (float)log(x);
+}
+
+float sinf(float x) {
+	return (float)sin(x);
+}
+
+float tanhf(float x) {
+	return (float)tanh(x);
+}
+
+float roundf(float x) {
+	return (float)round(x);
+}
+
+float ceilf(float x) {
+	return (float)ceil(x);
+}
+
+float floorf(float x) {
+	return (float)floor(x);
+}
+
+float fmodf(float x, float y) {
+	return (float)fmod(x, y);
+}
+
+#endif /* !defined(_MSC_VER) */
+#endif /* !__STDC_VERSION__ */
 #ifdef __cplusplus
 }
 #endif
